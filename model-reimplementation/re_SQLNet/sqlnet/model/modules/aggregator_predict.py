838e3108256009850be001c10469fecf379a811a
vincentlunc <xiaopeng@live.unc.edu>
2018-10-22 00:43:41 -0400

    Add predict

---
+++ model-reimplementation/re_SQLNet/sqlnet/model/modules/aggregator_predict.py
@@ -0,0 +1,8 @@
+import json
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+# from net_utils 
+
---
+++ model-reimplementation/re_SQLNet/sqlnet/model/modules/net_utils.py
@@ -0,0 +1,6 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.autograd import Variable
+
+def run_lstm()
